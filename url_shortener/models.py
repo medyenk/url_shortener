e79264bedc3dfdc2290b4e5cd821a243f78719e8
Medyen <medianak@hotmail.co.uk>
2020-06-02 17:46:39 +0100

    functionality finished

---
+++ url_shortener/models.py
@@ -0,0 +1,24 @@
+import string
+from .extensions import db
+from random import choices
+
+
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(512))
+    short_url = db.Column(db.String(3), unique=True)
+
+    def __init__(self, **kwargs):u
+        super().__init__(**kwargs)
+        self.short_url = self.generate_short_link()
+
+    def generate_short_link(self):
+        characters = string.digits + string.ascii_letters
+        short_url = ''.join(choices(characters, k=3))
+
+        link = self.query.filter_by(short_url=short_url).first()
+
+        if link:
+            return self.generate_short_link()
+       
+        return short_url
